151480e86752b98e5490df0a40a3abb80df227ac
Anthony Fernandez <Anthony.fernandez@alarabiya.net>
2022-04-08 17:05:57 +0400

    test

--- SonarCloudTest/ViewController.swift
+++ SonarCloudTest/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("hello world example")
+        print("hello world")
     }
 
 
